af903c84c989ff2ae7a3fdcf6043379dc784886b
Ananya Mishra <ananyam.hestabit@gmail.com>
2026-02-08 12:12:34 +0530

    Add Day3 assignment with bisect + stash proofs

---
+++ Week1/Day3/app.js
@@ -0,0 +1,10 @@
+console.log('H1')
+console.log('H2')
+console.log('H3')
+console.log('H4')
+console.log('H5'
+console.log('H6')
+console.log('H7')
+console.log('H8')
+console.log('H9')
+console.log('H10')
