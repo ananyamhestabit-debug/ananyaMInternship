ece0f2cf987f87b003aee0a1ca83cfeb42bd1111
Ananya Mishra <ananyam.hestabit@gmail.com>
2026-03-11 22:26:01 +0530

    week6/day4 added

--- Week4/Day1/src/models/Order.js
+++ Week4/Day1/src/models/Order.js
@@ -7,20 +7,24 @@ const orderSchema = new mongoose.Schema(
       ref: "Account",
       required: true,
     },
+
     productName: {
       type: String,
       required: true,
     },
+
     amount: {
       type: Number,
       required: true,
       min: 0,
     },
+
     status: {
       type: String,
       enum: ["pending", "completed", "cancelled"],
       default: "pending",
     },
+
     expiresAt: {
       type: Date,
     },
@@ -28,6 +32,7 @@ const orderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+//ttl index-->
 orderSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 module.exports = mongoose.model("Order", orderSchema);
